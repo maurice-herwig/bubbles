3a148441eba5616b5fdec1ed41ccedf238d84e94
Maurice Herwig <maurice.herwig@uni-kassel.de>
2026-05-30 11:04:01 +0200

    Refactor BufferedBisimulationGames variable names
    Rename short, ambiguous parameters and locals to descriptive names (state_pair->node_state_pair, w->buffer_word, i->automaton_index, m->move_type, etc.) across BufferedBisimulationGames. Update helper functions and call sites accordingly, clarify successor iteration variable names, and add an assertion ensuring MOVE nodes have a non-empty buffer. Changes are stylistic and improve readability and safety without altering core algorithmic behavior.

--- bubbles/BufferedBisimulationGames.py
+++ bubbles/BufferedBisimulationGames.py
@@ -21,9 +21,15 @@ class BufferedBisimulationGames(BisimulationGames):
         # =====================
         # auxiliary functions
         # =====================
-        def check_initial(state_pair: tuple, w: str, i: int, m: int):
+        def check_initial(node_state_pair: tuple, buffer_word: str, automaton_index: int, move_type: int):
             # TODO aufschrieb aktuell nur für ein startzustand
-            return state_pair[0] in initials[0] and state_pair[1] in initials[1] and w == '' and i == 0 and m == 0
+            return (
+                    node_state_pair[0] in initials[0]
+                    and node_state_pair[1] in initials[1]
+                    and buffer_word == ''
+                    and automaton_index == 0
+                    and move_type == 0
+            )
 
         finals = {i: self.automatons[i].get_finals() for i in range(2)}
         non_finals = {i: {s for s in range(self.automatons[i].get_number_of_states()) if s not in finals[i]}
@@ -52,8 +58,9 @@ class BufferedBisimulationGames(BisimulationGames):
 
             return False
 
-        def new_player1_node(state_pair: tuple, w: str, i: int, m: int):
-            new_node = (state_pair, w, i, m)
+        def new_player1_node(
+                node_state_pair: tuple, buffer_word: str, automaton_index: int, move_type: int):
+            new_node = (node_state_pair, buffer_word, automaton_index, move_type)
 
             if new_node in all_attractor_nodes:
                 return False
@@ -65,8 +72,9 @@ class BufferedBisimulationGames(BisimulationGames):
             new_attractor_nodes.add(new_node)
             return propagate_new_attractor_nodes([new_node])
 
-        def new_player2_node(state_pair: tuple, w: str, i: int, m: int):
-            new_node = (state_pair, w, i, m)
+        def new_player2_node(
+                node_state_pair: tuple, buffer_word: str, automaton_index: int, move_type: int):
+            new_node = (node_state_pair, buffer_word, automaton_index, move_type)
 
             if new_node in all_attractor_nodes:
                 return False
@@ -76,37 +84,48 @@ class BufferedBisimulationGames(BisimulationGames):
 
             successors_not_in_attractor = set()
 
-            if m == MOVES[FLUSH]:
-                used_automaton: FiniteAutomata = self.automatons[1 - i]
-                current_state = state_pair[1 - i]
+            if move_type == MOVES[FLUSH]:
+                used_automaton: FiniteAutomata = self.automatons[1 - automaton_index]
+                current_state = node_state_pair[1 - automaton_index]
 
                 # Compute all states that can be reached by flushing the buffer
-                successors = used_automaton.get_successors(s=current_state, a=w[0]) if w else {current_state}
-                for letter in w[1:]:
+                successors = (
+                    used_automaton.get_successors(s=current_state, a=buffer_word[0])
+                    if buffer_word
+                    else {current_state}
+                )
+                for letter in buffer_word[1:]:
                     successors = {
                         successor
-                        for s in successors
-                        for successor in used_automaton.get_successors(s=s, a=letter)
+                        for successor_state in successors
+                        for successor in used_automaton.get_successors(s=successor_state, a=letter)
                     }
 
                 # Compute all nodes that can be reached by flushing the buffer
                 for successor in successors:
-                    successor_state_pair = (state_pair[0], successor) if i == 0 else (successor, state_pair[1])
-                    successor_node = (successor_state_pair, '', 1 - i, MOVES[MOVE])
+                    successor_state_pair = (
+                        (node_state_pair[0], successor)
+                        if automaton_index == 0
+                        else (successor, node_state_pair[1])
+                    )
+                    successor_node = (successor_state_pair, '', 1 - automaton_index, MOVES[MOVE])
 
                     if successor_node not in all_attractor_nodes:
                         successors_not_in_attractor.add(successor_node)
 
-            elif m == MOVES[MOVE]:
-                if not w:
-                    return False
+            elif move_type == MOVES[MOVE]:
+                assert buffer_word, "Player-II MOVE nodes must have a non-empty buffer."
 
-                b = w[-1]
-                vv = w[:-1]
+                last_letter = buffer_word[-1]
+                remaining_word = buffer_word[:-1]
 
-                for q in self.automatons[1 - i].get_successors(s=state_pair[1 - i], a=b):
-                    new_state_pair = (state_pair[0], q) if i == 0 else (q, state_pair[1])
-                    successor_node = (new_state_pair, vv, i, MOVES[CHOICE])
+                for successor_state in self.automatons[1 - automaton_index].get_successors(
+                        s=node_state_pair[1 - automaton_index], a=last_letter):
+                    successor_node = ((
+                                          (node_state_pair[0], successor_state)
+                                          if automaton_index == 0
+                                          else (successor_state, node_state_pair[1])
+                                      ), remaining_word, automaton_index, MOVES[CHOICE])
 
                     if successor_node not in all_attractor_nodes:
                         successors_not_in_attractor.add(successor_node)
@@ -130,16 +149,26 @@ class BufferedBisimulationGames(BisimulationGames):
 
             return False
 
-        def add_moves_to_player1_node_for_choice_nodes(state_pair: tuple, aw: str, i: int):
-            if not aw:
+        def add_moves_to_player1_node_for_choice_nodes(
+                node_state_pair: tuple, extended_buffer_word: str, automaton_index: int):
+            if not extended_buffer_word:
                 return False
 
-            a = aw[0]
-            w = aw[1:]
+            first_letter = extended_buffer_word[0]
+            remaining_word = extended_buffer_word[1:]
 
-            for q in self.automatons[i].get_predecessors(s=state_pair[i], a=a):
-                new_state_pair = (q, state_pair[1]) if i == 0 else (state_pair[0], q)
-                if new_player1_node(state_pair=new_state_pair, w=w, i=i, m=MOVES[MOVE]):
+            for predecessor_state in self.automatons[automaton_index].get_predecessors(
+                    s=node_state_pair[automaton_index], a=first_letter):
+                if new_player1_node(
+                        node_state_pair=(
+                                (predecessor_state, node_state_pair[1])
+                                if automaton_index == 0
+                                else (node_state_pair[0], predecessor_state)
+                        ),
+                        buffer_word=remaining_word,
+                        automaton_index=automaton_index,
+                        move_type=MOVES[MOVE],
+                ):
                     return True
 
             return False
@@ -177,44 +206,71 @@ class BufferedBisimulationGames(BisimulationGames):
 
         # main loop
         while last_added_attractor_nodes:
-            for (state_pair, buffer_word, i, m) in last_added_attractor_nodes:
+            for (state_pair, current_buffer_word, i, m) in last_added_attractor_nodes:
 
                 # choice
                 if m == MOVES[CHOICE]:
-                    n = len(buffer_word)
+                    n = len(current_buffer_word)
                     if n == self.buffer_size:
                         # kann nur von Spieler 1 darhin gespiellt worden sein
-                        if add_moves_to_player1_node_for_choice_nodes(state_pair=state_pair, aw=buffer_word, i=i):
+                        if add_moves_to_player1_node_for_choice_nodes(
+                                node_state_pair=state_pair,
+                                extended_buffer_word=current_buffer_word,
+                                automaton_index=i,
+                        ):
                             return False, f'The automatas are not {self.buffer_size}-buffer equivalent'
 
                     elif n == (self.buffer_size - 1):
                         # kann von 1 oder 2 dahin gespielt worden sein
                         # case player 1
-                        if add_moves_to_player1_node_for_choice_nodes(state_pair=state_pair, aw=buffer_word, i=i):
+                        if add_moves_to_player1_node_for_choice_nodes(
+                                node_state_pair=state_pair,
+                                extended_buffer_word=current_buffer_word,
+                                automaton_index=i,
+                        ):
                             return False, f'The automatas are not {self.buffer_size}-buffer equivalent'
 
                         # case plyer 2
                         for q, a in self.automatons[1 - i].get_all_predecessors_with_letter(s=state_pair[1 - i]):
                             new_state_pair = (state_pair[0], q) if i == 0 else (q, state_pair[1])
-                            if new_player2_node(state_pair=new_state_pair, w=buffer_word + a, i=i, m=MOVES[MOVE]):
+                            if new_player2_node(
+                                    node_state_pair=new_state_pair,
+                                    buffer_word=current_buffer_word + a,
+                                    automaton_index=i,
+                                    move_type=MOVES[MOVE],
+                            ):
                                 return False, f'The automatas are not {self.buffer_size}-buffer equivalent'
 
                     elif (self.buffer_size - 1) > n > 0:
                         # kann nur von 1 dahin gespielt worden sein
-                        if add_moves_to_player1_node_for_choice_nodes(state_pair=state_pair, aw=buffer_word, i=i):
+                        if add_moves_to_player1_node_for_choice_nodes(
+                                node_state_pair=state_pair,
+                                extended_buffer_word=current_buffer_word,
+                                automaton_index=i,
+                        ):
                             return False, f'The automatas are not {self.buffer_size}-buffer equivalent'
 
                 elif m == MOVES[FLUSH]:
-                    if new_player1_node(state_pair=state_pair, w=buffer_word, i=i, m=MOVES[CHOICE]):
+                    if new_player1_node(
+                            node_state_pair=state_pair,
+                            buffer_word=current_buffer_word,
+                            automaton_index=i,
+                            move_type=MOVES[CHOICE],
+                    ):
                         return False, f'The automatas are not {self.buffer_size}-buffer equivalent'
 
                 elif m == MOVES[MOVE]:
                     # Variante 1 Spieler 1 hat dorthin gespielt
-                    if new_player1_node(state_pair=state_pair, w=buffer_word, i=i, m=MOVES[CHOICE]):
+                    if new_player1_node(
+                            node_state_pair=state_pair,
+                            buffer_word=current_buffer_word,
+                            automaton_index=i,
+                            move_type=MOVES[CHOICE],
+                    ):
                         return False, f'The automatas are not {self.buffer_size}-buffer equivalent'
 
                     # Variante 2 Spieler 2 hat den buffer geleert
-                    if buffer_word == '':
+                    if current_buffer_word == '':
 
                         # Alle Flush nodes bestimmen
                         used_automaton: FiniteAutomata = self.automatons[i]
@@ -233,7 +289,12 @@ class BufferedBisimulationGames(BisimulationGames):
                         # Für alle Flush nodes die möglichen nachfolger nodes bestimmen
                         for p, v in pos_flash:
                             new_state_pair = (p, state_pair[1]) if i == 0 else (state_pair[0], p)
-                            if new_player2_node(state_pair=new_state_pair, w=v, i=(1 - i), m=MOVES[FLUSH]):
+                            if new_player2_node(
+                                    node_state_pair=new_state_pair,
+                                    buffer_word=v,
+                                    automaton_index=(1 - i),
+                                    move_type=MOVES[FLUSH],
+                            ):
                                 return False, f'The automatas are not {self.buffer_size}-buffer equivalent'
 
             # Update for next iteration
